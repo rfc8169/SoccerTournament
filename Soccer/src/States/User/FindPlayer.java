510dbf2b62f668659fee746dbe6271960034be16
Benjamin Ezard <bwe4647@rit.edu>
2018-04-13 13:15:51 -0400

    FindPLayer SQL added

--- Soccer/src/States/User/FindPlayer.java
+++ Soccer/src/States/User/FindPlayer.java
@@ -4,11 +4,14 @@ import States.Role;
 import States.StateType;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class FindPlayer extends States.State {
     StringBuilder pathAppend = new StringBuilder("FindPlayer/");
     Scanner scanner = new Scanner(System.in);
+    Statement statement;
 
     public FindPlayer(Role role, Connection connection) {
         super(role, connection);
@@ -22,18 +25,39 @@ public class FindPlayer extends States.State {
         while (true) {
 
             System.out.println(modifiableData);
+            try{
+                statement = connection.createStatement();
+                String sql = "SELECT CONCAT(LAST_NAME,', ',First_name,', ',TEAM) FROM USER JOIN PLAYER ORDER BY Last_Name;";
+                ResultSet rs = statement.executeQuery(sql);
+                System.out.println("PLayers: Lname, Fname, Team");
+                while(rs.next()){
+                    System.out.println(rs.getString(1));
+                }
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
             System.out.println("try 'h' for help");
-            System.out.print("enter player: ");
+            System.out.print("enter player (LName FName): \n");
             input = scanner.nextLine();
-
-            //potentially do some work or actions:
-            //todo
 
             //determine appropriate return type:
             if (input.equals("")) return null;
             else if(input.equals("h")) help();
             else if(input.equals("e"))return StateType.END;
             else{
+                try{
+                    statement = connection.createStatement();
+                    String sql = "SELECT UID FROM user WHERE First_Name='"+input.split(" ")[0]+
+                            "' AND Last_Name='"+input.split(" ")+"';";
+                    ResultSet rs = statement.executeQuery(sql);
+                    while(rs.next()){
+                        input = rs.getString(1);
+                    }
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
                 input = "<"+input+">/";
                 pathAppend.append(input);
                 modifiableData.append(input);
